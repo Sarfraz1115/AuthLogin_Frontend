9d7d98ec8a867640bff9728aecb0b93f17840bf2
Sarfraz1115 <syedsarfraz907@gmail.com>
2025-06-01 10:31:30 +0530

    change axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInsta = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://authlogin-backend.onrender.com',
    withCredentials: true,
 })
